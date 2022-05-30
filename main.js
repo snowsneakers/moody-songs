66496c54064a3f9cca001e772aaf323f8e23b5dd
patrick snowden <patrickbsnowden@outlook.com>
2022-05-30 16:56:52 -0700

    used oop, cleaned it up, made it waaaayyy dryer, and added comments

--- main.js
+++ main.js
@@ -1,53 +1,76 @@
-makeReq();
+//On change of dropdown menu it runs runs makeReq funciton
+document.querySelector("select").addEventListener("change", makeReq);
 
+//based on selection, function fetches api from server
 async function makeReq() {
-  const res = await fetch("/api");
+    //stores value of select element in variable
+  let mood = document.querySelector("select").value;
+
+  //fetches api from server
+  const res = await fetch(`/api/${mood}`);
+  //stores data from fetch
   const data = await res.json();
   console.log(data);
-  function blah(index) {
+  let random = Math.floor(Math.random() * data.length);
+
+  
+  const song = new Songs(data[random].url, data[random].artist, data[random].song, data[random].lyrics);
+  
+  //clears previous song from container
+  song.clear();
+
+  //dpending on what value is selected calls getSongInfo function
+  switch (mood) {
+    case "happy":
+      song.getSongInfo();
+      break;
+    case "angry":
+      song.getSongInfo();
+      break;
+    case "sad":
+      song.getSongInfo();
+      break;
+    case "chill":
+      song.getSongInfo();
+      break;
+    case "inLove":
+      song.getSongInfo();
+      break;
+  }
+}
+
+//creates song class
+class Songs {
+    //parameters are what values from api obj we want
+  constructor(url, artist, song, lyrics) {
+    this.url = url;
+    this.artist = artist;
+    this.song = song;
+    this.lyrics = lyrics;
+  }
+
+  //clears container
+  clear() {
+    const albumInfo = document.querySelector(".albumInfo");
+    while (albumInfo.firstChild) {
+      albumInfo.removeChild(albumInfo.firstChild);
+    }
+  }
+
+  //function creates div, div conentent gets added using fetched data, appends div to DOM
+  getSongInfo() {
     const div = document.createElement("div");
     div.classList.add("albumStuff");
     div.innerHTML = `
-            <div>
-            <iframe frameBorder="0" src="${data[index].url}"></iframe>
-            <h1>${data[index].artist}</h1>
-            <h2>${data[index].song}</h2>
-            </div>
-            <div class="lyrics">
-            <p>${data[index].lyrics}</p>
-            </div>
-        `;
+                <div>
+                    <iframe frameBorder="0" src="${this.url}"></iframe>
+                    <h1>${this.artist}</h1>
+                    <h2>${this.song}</h2>
+                </div>
+                <div class="lyrics">
+                    <p>${this.lyrics}</p>
+                </div>
+            `;
     document.querySelector(".albumInfo").appendChild(div);
-  }
-
-  document.querySelector("select").addEventListener("change", () => {
-    console.log(document.querySelector("select").value);
-    let emo = document.querySelector("select").value;
-    clear();
-
-    switch (emo) {
-      case "happy":
-        blah(0);
-        break;
-      case "angry":
-        blah(1);
-        break;
-      case "sad":
-        blah(3);
-        break;
-      case "chill":
-        blah(2);
-        break;
-      case "inLove":
-        blah(4);
-        break;
-    }
-  });
-}
-
-function clear() {
-  const albumInfo = document.querySelector(".albumInfo");
-  while (albumInfo.firstChild) {
-    albumInfo.removeChild(albumInfo.firstChild);
   }
 }
