46cfd48d7d5aa6b12b93c4a34e748ed51f439f29
patrick snowden <patrickbsnowden@outlook.com>
2022-05-30 13:31:53 -0700

    changed backend

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-makeReq()
+makeReq();
 
-async function makeReq(){
-    const res = await fetch('/api')
-    const data = await res.json()
-    console.log(data)
-    function mood(index){
-        const div = document.createElement('div')
-        div.classList.add('albumStuff')
-        div.innerHTML = `
+async function makeReq() {
+  const res = await fetch("/api");
+  const data = await res.json();
+  console.log(data);
+  function blah(index) {
+    const div = document.createElement("div");
+    div.classList.add("albumStuff");
+    div.innerHTML = `
             <div>
             <iframe frameBorder="0" src="${data[index].url}"></iframe>
-            <h2>${data[index].artist}</h2>
-            <h3>${data[index].song}</h3>
+            <h1>${data[index].artist}</h1>
+            <h2>${data[index].song}</h2>
             </div>
             <div class="lyrics">
             <p>${data[index].lyrics}</p>
             </div>
-        `
-        document.querySelector('.albumInfo').appendChild(div)
-    }
+        `;
+    document.querySelector(".albumInfo").appendChild(div);
+  }
 
-    document.querySelector('select').addEventListener('change', () => {
-        console.log(document.querySelector('select').value)
-        let emo = document.querySelector('select').value
-        clear()
-    
-        if(emo == 'happy' ){
-            mood(0)
-        } else if(emo == 'angry'){
-            mood(1)
-        } else if(emo == 'sad'){
-            mood(3)
-        } else if(emo == 'chill'){
-            mood(2)
-        } else if(emo == 'inLove'){
-            mood(4)
-        }
-    })
+  document.querySelector("select").addEventListener("change", () => {
+    console.log(document.querySelector("select").value);
+    let emo = document.querySelector("select").value;
+    clear();
+
+    switch (emo) {
+      case "happy":
+        blah(0);
+        break;
+      case "angry":
+        blah(1);
+        break;
+      case "sad":
+        blah(3);
+        break;
+      case "chill":
+        blah(2);
+        break;
+      case "inLove":
+        blah(4);
+        break;
+    }
+  });
 }
 
-function clear(){
-        const albumInfo = document.querySelector(".albumInfo");
-        while (albumInfo.firstChild) {
-            albumInfo.removeChild(albumInfo.firstChild);
-        }
+function clear() {
+  const albumInfo = document.querySelector(".albumInfo");
+  while (albumInfo.firstChild) {
+    albumInfo.removeChild(albumInfo.firstChild);
+  }
 }
