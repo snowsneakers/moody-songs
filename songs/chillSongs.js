d7868ec0a559ab8e84988678d9761745b36038e2
patrick snowden <patrickbsnowden@outlook.com>
2022-05-30 17:25:17 -0700

    formatted song lyrics

--- songs/chillSongs.js
+++ songs/chillSongs.js
@@ -1,7 +1,7 @@
 const chillSongsObj = {
   chillSongs: [
     {
-      artist: "Various Artist",
+      artist: "Various Artists",
         song: "Various Songs",
         url: "https://www.youtube.com/embed/n61ULEU7CO0",
         lyrics: `Instrumental`
